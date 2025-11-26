5518faef2da2f78f43dbdbabf371c4df2c5b952d
brianykak24-netizen <brianyk_ak24@nuwm.edu.ua>
2025-11-27 00:56:02 +0200

    wqe

---
+++ LabWork/GraphRenderer.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System;
+using System.Linq;
+
+namespace LabWork
+{
+    public class GraphRenderer : IDisposable
+    {
+        private readonly Pen _gridPen;
+        private readonly Pen _axisPen;
+        private readonly Pen _graphPen;
+        private readonly Brush _pointBrush;
+        private readonly Font _font;
+        private readonly Brush _textBrush;
+
+        public GraphRenderer()
+        {
+            _gridPen = new Pen(Color.LightGray, 1) { DashStyle = System.Drawing.Drawing2D.DashStyle.Dot };
+            _axisPen = new Pen(Color.Black, 2);
+            _graphPen = new Pen(Color.Blue, 2);
+            _pointBrush = new SolidBrush(Color.Red);
+            _textBrush = Brushes.Black;
+            _font = new Font("Arial", 8);
+        }
+
+        public void Draw(Graphics g, RectangleF clientRect, List<PointF> dataPoints, bool showAsScatter)
+        {
+            if (dataPoints == null || dataPoints.Count < 2) return;
+
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            // 1. Визначення меж (World Coordinates)
+            float minX = dataPoints.Min(p => p.X);
+            float maxX = dataPoints.Max(p => p.X);
+            float minY = dataPoints.Min(p => p.Y);
+            float maxY = dataPoints.Max(p => p.Y);
+
+            // Додаємо трохи "повітря" по краях
+            float margin = 40f;
+            RectangleF drawArea = new RectangleF(
+                clientRect.X + margin,
+                clientRect.Y + margin,
+                clientRect.Width - 2 * margin,
+                clientRect.Height - 2 * margin);
+
+            // 2. Обчислення коефіцієнтів масштабування
+            float scaleX = drawArea.Width / (maxX - minX);
+            float scaleY = drawArea.Height / (maxY - minY);
+
+            // Локальна функція для перетворення координат (World -> Screen)
+            PointF ToScreen(PointF point)
+            {
+                float screenX = drawArea.Left + (point.X - minX) * scaleX;
+                // Інвертуємо Y, бо в WinForms вісь Y йде вниз
+                float screenY = drawArea.Bottom - (point.Y - minY) * scaleY;
+                return new PointF(screenX, screenY);
+            }
+
+            // 3. Малювання сітки та осей
+            g.DrawRectangle(_gridPen, drawArea.X, drawArea.Y, drawArea.Width, drawArea.Height);
+
+            // Малюємо підписи меж
+            g.DrawString($"X: {minX:F1}", _font, _textBrush, drawArea.Left, drawArea.Bottom + 5);
+            g.DrawString($"X: {maxX:F1}", _font, _textBrush, drawArea.Right - 30, drawArea.Bottom + 5);
+            g.DrawString($"Y: {minY:F2}", _font, _textBrush, drawArea.Left - 35, drawArea.Bottom - 10);
+            g.DrawString($"Y: {maxY:F2}", _font, _textBrush, drawArea.Left - 35, drawArea.Top);
+
+            // 4. Малювання графіка
+            // Перетворюємо всі точки заздалегідь
+            PointF[] screenPoints = dataPoints.Select(p => ToScreen(p)).ToArray();
+
+            if (showAsScatter)
+            {
+                // Точковий графік (Scatter Plot)
+                float pointSize = 6;
+                foreach (var p in screenPoints)
+                {
+                    g.FillEllipse(_pointBrush, p.X - pointSize / 2, p.Y - pointSize / 2, pointSize, pointSize);
+                }
+            }
+            else
+            {
+                // Лінійний графік (Line Plot)
+                g.DrawLines(_graphPen, screenPoints);
+            }
+        }
+
+        public void Dispose()
+        {
+            _gridPen.Dispose();
+            _axisPen.Dispose();
+            _graphPen.Dispose();
+            _pointBrush.Dispose();
+            _font.Dispose();
+        }
+    }
+}
---
+++ LabWork/MainForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LabWork
+{
+    public partial class MainForm : Form
+    {
+        private readonly FunctionCalculator _calculator;
+        private readonly GraphRenderer _renderer;
+        private List<PointF> _dataPoints;
+
+        // UI елементи
+        private CheckBox _chkScatterMode;
+        private Panel _controlPanel;
+
+        public MainForm()
+        {
+            // Налаштування форми
+            this.Text = "Лабораторна робота: Графік";
+            this.Size = new Size(800, 600);
+
+            // 1. Увімкнення подвійної буферизації (вимога проти мерехтіння)
+            this.DoubleBuffered = true;
+
+            // Ініціалізація компонентів
+            InitializeCustomComponents();
+
+            // Ініціалізація логіки
+            _calculator = new FunctionCalculator(2.5, 9, 0.8);
+            _renderer = new GraphRenderer();
+
+            // Завантаження даних (Model)
+            LoadData();
+
+            // Підписка на події
+            this.Resize += (s, e) => this.Invalidate(); // Перемальовуємо при зміні розміру
+            this.Paint += MainForm_Paint;
+        }
+
+        private void InitializeCustomComponents()
+        {
+            // Панель для кнопок знизу
+            _controlPanel = new Panel
+            {
+                Dock = DockStyle.Bottom,
+                Height = 50,
+                BackColor = Color.WhiteSmoke
+            };
+
+            // Чекбокс для перемикання режиму (Scatter vs Line)
+            _chkScatterMode = new CheckBox
+            {
+                Text = "Точковий режим (Scatter Plot)",
+                AutoSize = true,
+                Location = new Point(20, 15)
+            };
+
+            // При зміні чекбокса просто викликаємо перемалювання
+            _chkScatterMode.CheckedChanged += (s, e) => this.Invalidate();
+
+            _controlPanel.Controls.Add(_chkScatterMode);
+            this.Controls.Add(_controlPanel);
+        }
+
+        private void LoadData()
+        {
+            _dataPoints = _calculator.GetPoints();
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            // Обчислюємо область для малювання (все вікно мінус панель керування)
+            RectangleF plotArea = new RectangleF(0, 0, this.ClientSize.Width, this.ClientSize.Height - _controlPanel.Height);
+
+            // Делегуємо малювання рендереру
+            // Передаємо стан чекбокса (Checked) для вибору типу графіка
+            _renderer.Draw(e.Graphics, plotArea, _dataPoints, _chkScatterMode.Checked);
+        }
+
+        // Очищення ресурсів при закритті
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            _renderer.Dispose();
+            base.OnFormClosed(e);
+        }
+    }
+}
